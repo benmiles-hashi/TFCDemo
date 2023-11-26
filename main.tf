c984a2e3bed7a0075822a48087acbe3ba174cc6b
bennycdr <58797411+bennycdr@users.noreply.github.com>
2023-11-26 17:03:31 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "myec2" {
   ami                     = "ami-0230bd60aa48260c6"
-  instance_type           = "t2.micro"
+  instance_type           = "t3.micro"
 
   tags = {
     Name = "MyEC2-2"
