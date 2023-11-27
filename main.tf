d859c7ca2404d9ddea51f0d2279c6da3ef5c68c9
bennycdr <58797411+bennycdr@users.noreply.github.com>
2023-11-27 13:05:06 -0500

    changed to t3 micro

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "myec2" {
   ami                     = var.ami
-  instance_type           = "t2.micro"
+  instance_type           = "t3.micro"
 
   tags = {
     Name = "MyEC2b"
