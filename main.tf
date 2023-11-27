edfbd31387ad51005716d69c245ef281a8c83485
bennycdr <58797411+bennycdr@users.noreply.github.com>
2023-11-27 14:03:17 -0500

    reset to m5.large

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "myec2" {
   ami                     = var.ami
-  instance_type           = "t2.micro"
+  instance_type           = "m5.large"
 
   tags = {
     Name = "MyEC2-VCS"
