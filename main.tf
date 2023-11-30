f1ad5edaf0453478f8436888d47c2cdc8f90e98d
bennycdr <58797411+bennycdr@users.noreply.github.com>
2023-11-30 13:20:48 -0500

    update to t2.micro and add owner tag

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "myec2" {
   ami                     = var.ami
-  instance_type           = "m5.xlarge"
+  instance_type           = "t2.micro"
 
   tags = {
     Name = "MyEC2-VCS"
+    owner = "Ben Miles"
   }
 
 }
