535dd948277cc06b1cb5da6accf65ca28a5047c4
benmiles-hashi <ben.miles@hashicorp.com>
2024-01-31 21:29:49 -0500

    updated aws variables

--- ec2-staging/variables.tf
+++ ec2-staging/variables.tf
@@ -1,5 +1,5 @@
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
+#variable "AWS_ACCESS_KEY_ID" {}
+#variable "AWS_SECRET_ACCESS_KEY" {}
 variable "ami" {
     default = "ami-0277155c3f0ab2930"
 }
