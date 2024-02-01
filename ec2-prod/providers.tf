bbbbaa22b554fe3b7dee1d64afd2d9ade527d96a
benmiles-hashi <ben.miles@hashicorp.com>
2024-01-31 21:18:41 -0500

    updated aws variable names

--- ec2-prod/providers.tf
+++ ec2-prod/providers.tf
@@ -8,6 +8,6 @@ terraform {
 }
 provider "aws" {
   region = "us-east-1"
-  access_key = var.AWS_ACCESS_ID
-  secret_key = var.AWS_SECRET
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
