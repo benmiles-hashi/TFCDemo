86ff457d54e45ba3dee317191b4f0a89eceef31b
benmiles-hashi <ben.miles@hashicorp.com>
2024-02-01 10:26:00 -0500

    changed instance type to t2.micro

--- ec2-prod/main.tf
+++ ec2-prod/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "myec2" {
   ami                     = var.ami
-  instance_type           = "m5.xlarge"
+  instance_type           = "t2.micro"
 
   tags = {
     Name = "MyEC2-VCS"
