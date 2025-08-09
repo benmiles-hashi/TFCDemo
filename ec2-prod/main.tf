059989d9494f7afb5b0e662191dc17e2165c925d
Ben Miles <155984943+benmiles-hashi@users.noreply.github.com>
2025-08-09 11:24:13 -0600

    Update main.tf to t2 micro

--- ec2-prod/main.tf
+++ ec2-prod/main.tf
@@ -1,10 +1,9 @@
 resource "aws_instance" "myec2" {
   ami                     = var.ami
-  instance_type           = "t5.Large"
+  instance_type           = "t2.micro"
 
   tags = {
     Name = "MyEC2-VCS"
-    owner = "Ben Miles"
     Env = "Prod"
   }
 
